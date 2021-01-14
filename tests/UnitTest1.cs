66071938acb914cd6fd27df4e41ed1bce8c91ed8
Sytabram <bryan.zweiacker@gmail.com>
2021-01-14 10:48:52 +0100

    - Projet de base avec Tests

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieAPI.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
